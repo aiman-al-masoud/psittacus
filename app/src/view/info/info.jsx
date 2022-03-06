59dfc2504f34d1a2319da8c9a38e65fb375171e3
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-03-06 17:14:45 +0100

    link to demo and full license terms

--- app/src/view/info/info.jsx
+++ app/src/view/info/info.jsx
@@ -17,10 +17,17 @@ export default class Info extends Component {
                 <p>{L.win_linux_save_offline}</p>
                 <p>{L.mac_save_offline}</p>
 
+                <h1>{L.demo}</h1>
+                <a href={L.demo_link} target="_blank">{L.demo_link}</a>
+
                 <h1>{L.link_to_source_code_title}</h1>
                 <a href={L.link_to_source_code} target="_blank">{L.link_to_source_code}</a>
-
+               
+                <h1>{L.license}</h1>
                 <p>{L.license_notice}</p>
+                <h1>{L.full_terms}</h1>
+                <a href={L.license_link} target="_blank">{L.license_link}</a>
+
 
             </div>
 
