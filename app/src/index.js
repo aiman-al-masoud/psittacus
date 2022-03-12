4aa9292d8704cfda25135531a4923fe386244625
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-03-12 13:12:47 +0100

    refactoring

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import reactDOM from "react-dom";
 import App from "./view/app.jsx";
 import FaviconImage from "../res/favicon.png"
+import L from "./model/language.js";
 
 //set favicon
 let link = document.createElement('link')
@@ -9,7 +10,12 @@ link.rel = 'shortcut icon';
 link.href = FaviconImage;
 document.head.appendChild(link);
 
+//set title
+let title = document.createElement("title")
+title.innerHTML = L.app_name
+document.head.appendChild(title)
 
+//start the app
 reactDOM.render(<App/>, document.getElementById("root"))
 
 
