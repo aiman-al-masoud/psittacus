0c009591c3d5058f361d92d5db59d33da852e916
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-03-31 12:55:45 +0200

    refactoring Leitner

--- app/src/model/scheduler/Leitner.js
+++ app/src/model/scheduler/Leitner.js
@@ -10,55 +10,33 @@ export default class Leitner extends Scheduler{
 
     constructor(lessonJson){
         super(lessonJson)
-        this.loadBoxes() 
-        this.newBoxes = [[], []] //for the next time this lesson is taken
-        
+        this.initSequence()     
         this.counter = 0
-        this.highPriorityBox = this.boxes[0]
-        this.lowPriorityBox = this.boxes[1]
-        this.allPropositionHashes = this.highPriorityBox.concat(this.lowPriorityBox)
         this.current = this.getPropositionByHash(this.allPropositionHashes[0])
-
     }
 
-    loadBoxes(){
+    initSequence(){
 
-        
         try{
-            
-            this.boxes = [[],[]]
-            let propoScores = Leitner.lessonScores()[this.lessonId].propositions
 
-            console.log(propoScores, "proposcores")
+            let boxes = [[],[]]
+            let propoScores = Leitner.lessonScores()[this.lessonId].propositions
 
             propoScores.forEach((p)=>{
                 if (p[1] <= Proposition.MIN_PASSING_SCORE){
-                    this.boxes[0].push(p[0])
+                    boxes[0].push(p[0]) //high priority
                 }else{
-                    this.boxes[1].push(p[0])
+                    boxes[1].push(p[0]) //low priority
                 }
             })
 
+            this.allPropositionHashes = boxes[0].concat(boxes[1])
+
         }catch{
-            this.boxes = this.initBoxes()
+            this.allPropositionHashes =  this.propositions.map((p)=>{return p.getHash()})
         }
     }
 
-
-
-    /**
-     * First time ever a lesson is played, 
-     * no stored data yet, go with lesson order.
-     */
-    initBoxes(){
-        //two boxes. 0: high priority, 1: student already knows these
-        let newBoxes = [[], []]
-        let propoHashes = this.propositions.map((p)=>{return p.getHash()})
-        let partTwo = propoHashes.splice(propoHashes.length/2)
-        newBoxes[0]  = propoHashes
-        newBoxes[1]  = partTwo
-        return newBoxes
-    }
 
     /**
      * Get a Proposition by its hash.
@@ -76,14 +54,6 @@ export default class Leitner extends Scheduler{
 
     next(){
 
-        //student screwed up in previous:
-        if(this.current.getScore() < Proposition.MIN_PASSING_SCORE){
-            this.newBoxes[0].push(this.current.getHash())
-        }else{
-            this.newBoxes[1].push(this.current.getHash())
-        }
-
-        //point to next
         this.counter++
         this.current = this.getPropositionByHash(this.allPropositionHashes[this.counter]) 
         if (!this.current){
@@ -92,13 +62,6 @@ export default class Leitner extends Scheduler{
         }
        
     }
-
-
-
-
-
-
-
 
 
     static userProgress(){
@@ -114,11 +77,6 @@ export default class Leitner extends Scheduler{
         p.lesson_scores[lessonId] = data
         localStorage.setItem("user_progress", JSON.stringify(p))
     }
-
-    
-
-
-  
 
 
 }
