53ead025a401cc797c92c56b59ad129d899ab9ca
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-04-01 18:06:38 +0200

    no need for getPropositionByHash

--- app/src/model/scheduler/Leitner.js
+++ app/src/model/scheduler/Leitner.js
@@ -13,7 +13,7 @@ export default class Leitner extends Scheduler{
         super(lessonJson)
         this.initSequence()     
         this.counter = 0
-        this.current = this.getPropositionByHash(this.sequence[0])
+        this.current = this.propositions[this.counter]
     }
 
     /**
@@ -23,26 +23,19 @@ export default class Leitner extends Scheduler{
 
         try{
             let propoScores = UserProgress.lessonScores()[this.lessonId].propositions
-            propoScores = propoScores.sort((p1, p2)=> {return p1[1] - p2[1] } )
-            this.sequence = propoScores.map((p)=>{return p[0]})
-        }catch{
-            this.sequence =  this.propositions.map((p)=>{return p.getHash()})
-        }
-    }
+            propoScores = propoScores.sort((p1, p2)=> {return p1[1] - p2[1] } ) //sort by score (index=1)            
+            let hashes = propoScores.map((p)=>{return p[0]}) //get hashes (index=0)
+            this.propositions = this.propositions.sort((p1, p2)=>{ return hashes.indexOf(p1.getHash()) - hashes.indexOf(p2.getHash())    }  )
 
-    /**
-     * Get a Proposition by its hash.
-     * @param {number} propoHash 
-     * @returns {Proposition}
-     */
-    getPropositionByHash(propoHash){
-        return this.propositions.filter((p)=>{ return p.getHash()==propoHash })[0]
+        }catch{
+            //first time user takes lesson w/ this lessonId
+        }
     }
 
     next(){
 
         this.counter++
-        this.current = this.getPropositionByHash(this.sequence[this.counter]) 
+        this.current = this.propositions[this.counter]
         if (!this.current){
             this.current = Proposition.NULL
             this.isLessonOver = true
