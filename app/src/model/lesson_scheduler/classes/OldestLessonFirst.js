4ae1a087ee3a1f15545248ef593bf897ae187dd7
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-04-17 11:00:51 +0200

    added test OldestLessonFirst LessonScheduler

---
+++ app/src/model/lesson_scheduler/classes/OldestLessonFirst.js
@@ -0,0 +1,18 @@
+import Lesson from "../../lesson/Lesson";
+import LessonScheduler from "../LessonScheduler";
+
+export default class OldestLessonFirst extends LessonScheduler{
+
+    constructor(){
+        super()
+        this.lessonsScores = this.lessonsScores.sort( (s1, s2) => {return s1.last_taken - s2.last_taken  } )  
+        // console.log(this.lessonsScores)
+    }
+
+    async next(){
+        this.lessonsScores = this.lessonsScores.sort( (s1, s2) => {return s1.last_taken - s2.last_taken  } )  
+        let x = this.lessonsScores[0]
+        return await Lesson.getCachedLessonById(x.id)
+    }
+
+}
