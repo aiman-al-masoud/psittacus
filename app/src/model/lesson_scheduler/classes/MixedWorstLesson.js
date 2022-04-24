3a51f9d85448d08c0ea5b2a2369113a23c7985b1
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-04-24 14:57:55 +0200

    removed comments

--- app/src/model/lesson_scheduler/classes/MixedWorstLesson.js
+++ app/src/model/lesson_scheduler/classes/MixedWorstLesson.js
@@ -1,7 +1,5 @@
-import Lesson from "../../lesson/Lesson";
 import MixedLesson from "../../lesson/MixedLesson";
 import Proposition from "../../lesson/proposition/Proposition";
-import PropositionSchedulerBuilder from "../../lesson/proposition_scheduler/PropositionSchedulerBuilder";
 import LessonScheduler from "../LessonScheduler";
 
 /**
@@ -18,28 +16,17 @@ export default class MixedWorstLesson extends LessonScheduler {
     }
 
     async next() {
-        // this.lessonsScores = this.lessonsScores.sort( (s1, s2) => {return s1.overall - s2.overall  } )  //sort by score
-        // this.lessonsScores.splice(5) //worst n lessons
-        // let hashes = this.lessonsScores.flatMap(lez=>lez.propositions).sort((p1, p2)=>{return p1[1] - p2[1]  }).filter(p=>{return p[1] < Proposition.MIN_PASSING_SCORE   }).flatMap(p=>p[0])   //worst propositions
-        // let lessonIds = this.lessonsScores.map(l=>l.id)
-        // let lessons =  []
-
-        // for(let id of lessonIds){
-        //     lessons.push(   await Lesson.getCachedLessonById(id))
-        // }
-
-        // let propositions = lessons.flatMap(l=>l.propositions)
-        // propositions = propositions.filter(p=>{return  hashes.includes(p.getHash()) })
-        // propositions = propositions.sort(() => Math.random() - 0.5) //shuffle
-
-        // let newLesson = await Lesson.getCachedLessonById(lessonIds[0])
-        // newLesson.propositions = propositions
-        // newLesson.scheduler = PropositionSchedulerBuilder.getScheduler(undefined, propositions)
-        // return newLesson
+        
+        this.lessonsScores = this.lessonsScores.sort( (s1, s2) => {return s1.overall - s2.overall  } )  //sort by score
+        let n = 5
+        this.lessonsScores.splice(n) //worst n lessons
 
         let ml = new MixedLesson()
-        await ml.addLesson("capraitalianenglishprima", [3390])
-        await ml.addLesson("capraitalianenglishseconda", [2851 /*,946*/])
+
+        for(let s of this.lessonsScores){
+            let hashes = s.propositions.filter(p=>{return p[1] < Proposition.MIN_PASSING_SCORE  }).map(p=>p[0])               //sort((p1, p2)=>{return p1[1] - p2[1]  })
+            await ml.addLesson(s.id,  hashes)
+        }
         
         return ml
     }
