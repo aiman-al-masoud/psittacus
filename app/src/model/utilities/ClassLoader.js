063501be9c7400f2ff006154cccf7e93e057e38e
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-04-27 18:48:01 +0200

    comments

--- app/src/model/utilities/ClassLoader.js
+++ app/src/model/utilities/ClassLoader.js
@@ -1,6 +1,12 @@
 
 export default class ClassLoader{
 
+    /**
+     * Dynamically loads a class from its source code 
+     * and returns it.
+     * @param {string} sourceCodeString 
+     * @returns {Promise <object> }
+     */
     async fromSourceCode(sourceCodeString){
 
         let script = document.createElement('script');
@@ -10,6 +16,11 @@ export default class ClassLoader{
         return eval(classname)
     }
 
+    /**
+     * Extracts the name of a class from its source code.
+     * @param {string} sourceCodeString 
+     * @returns {string}
+     */
     static classnameFromSourceCode(sourceCodeString){
         return sourceCodeString.match(/class\s+(.*?){/)[1]
     }
