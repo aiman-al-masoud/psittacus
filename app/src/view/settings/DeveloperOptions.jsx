a9f594684cc4d8f84bf22c1cf8c49216c2a7b572
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-04-28 13:55:41 +0200

    adding enable dev options...

--- app/src/view/settings/DeveloperOptions.jsx
+++ app/src/view/settings/DeveloperOptions.jsx
@@ -2,17 +2,27 @@ import React, { Component } from "react";
 import PropositionSchedulerBuilder from "../../model/lesson/proposition_scheduler/PropositionSchedulerBuilder.js";
 import L from "../../model/utilities/Language.js";
 import { readText } from "../../model/utilities/Utils.js";
+import Styles from "../Styles.js";
 
 export default class DeveloperOptions extends Component {
-    
-    addCustomPropositionScheduler = async ()=>{
+
+    addCustomPropositionScheduler = async () => {
         let sourceCode = await readText()
         PropositionSchedulerBuilder.addCustomScheduler(sourceCode)
         PropositionSchedulerBuilder.loadCustomSchedulers()
     }
-    
-    render(){
+
+    render() {
         return (<div>
+
+            <span>Enable:</span>
+            <input type="checkbox" />
+
+            <br />
+
+            <h2>Load custom code</h2>
+            <div className="text_tip">Running code from untrusted sources is DANGEROUS: make sure you know what you're doing!</div>
+
             <button onClick={this.addCustomPropositionScheduler}>Add Custom Proposition Scheduler</button>
 
         </div>)
