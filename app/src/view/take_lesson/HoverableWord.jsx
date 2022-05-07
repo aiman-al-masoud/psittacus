c3d43395d954d3232195bb1928277f7fbc2face5
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-05-07 18:16:53 +0200

    cleanup

--- app/src/view/take_lesson/HoverableWord.jsx
+++ app/src/view/take_lesson/HoverableWord.jsx
@@ -19,17 +19,13 @@ export default class HoverableWord extends Component {
         return (
             <span>
                 <span onMouseLeave={this.toggleDefinition}   onMouseEnter={this.toggleDefinition}  style={ {color : this.state.hidden?"black": "red"  , cursor:"default"  } }   >{this.props.word} </span>
-                
-                {/* <span style={{ ...this.state.hidden? Styles.invisible:Styles.visibleInline, color : "red" }}>{this.props.definition} </span> */}
-                
-                <div style={{ ...this.state.hidden? Styles.invisible:Styles.visibleInline, position:"absolute", top:"top", border:"solid 1px black", borderRadius:"10px", width : "20vw", padding:"1vw",          background :  "rgba(0, 0, 255, 0.9)", color:"white" }}>   <p style={{ textDecoration: "underline"  }} >{this.props.word}</p>   <p>{this.props.definition}</p> </div>
-
+                                
+                <div  className="hovering_tool_tip" style={{ ...this.state.hidden? Styles.invisible:Styles.visibleInline}}>   <p style={{ textDecoration: "underline"  }} >{this.props.word}</p>   <p>{this.props.definition}</p> </div>
 
             </span>)
     }
 
     toggleDefinition = (e) => {
-        console.log(e)
         this.setState({hidden: !this.state.hidden})
     }
 
