67468a3d06eebe254f6fa3b7371ef7d775598a37
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-05-07 19:29:11 +0200

    ...

--- app/src/view/take_lesson/Tooltip.jsx
+++ app/src/view/take_lesson/Tooltip.jsx
@@ -6,13 +6,17 @@ import Styles from "../Styles.js";
  */
 export default class Tooltip extends Component {
 
+    /**
+     * 
+     * @param {{x:number, y:number, title:string, body:string}} props 
+     */
     constructor(props) {
         super(props)
     }
 
     render() {
         return (
-            <span className="hovering_tool_tip" style={ {left: this.props.x, top:this.props.y , ...(this.props.hidden ? Styles.invisible : Styles.visible)} }>
+            <span className="hovering_tool_tip" style={{ left: this.props.x, top: this.props.y, ...(this.props.hidden ? Styles.invisible : Styles.visible) }}>
                 <h1>{this.props.title}</h1>
                 <p>{this.props.body}</p>
             </span>
