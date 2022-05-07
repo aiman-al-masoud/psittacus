4cfd60a202de478fc5da94391af5a2a4bcb271cf
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-05-07 19:03:46 +0200

    better tooltip

--- app/src/view/take_lesson/HoverableWord.jsx
+++ app/src/view/take_lesson/HoverableWord.jsx
@@ -7,6 +7,8 @@ export default class HoverableWord extends Component {
 
     constructor(props) {
         super(props)
+
+        this.toolTip  = React.createRef()
         
         this.state = {
             hidden : true
@@ -17,15 +19,22 @@ export default class HoverableWord extends Component {
     render() {
         
         return (
-            <span>
-                <span onMouseLeave={this.toggleDefinition}   onMouseEnter={this.toggleDefinition}  style={ {color : this.state.hidden?"black": "red"  , cursor:"default"  } }   >{this.props.word} </span>
+            <span >
+                <span onMouseLeave={this.toggleDefinition}   onMouseEnter={this.toggleDefinition}   onClick={this.toggleDefinition} style={ {color : this.state.hidden?"black": "orange"  , cursor:"default"  } }   >{this.props.word} </span>
                                 
-                <div  className="hovering_tool_tip" style={{ ...this.state.hidden? Styles.invisible:Styles.visibleInline}}>   <p style={{ textDecoration: "underline"  }} >{this.props.word}</p>   <p>{this.props.definition}</p> </div>
+                <div ref={this.toolTip} className="hovering_tool_tip" style={{ ...this.state.hidden? Styles.invisible:Styles.visibleInline     }}>   <h1>{this.props.word}</h1>     <p>{this.props.definition}</p>    </div>
 
             </span>)
     }
 
     toggleDefinition = (e) => {
+        // console.log()
+        console.log(e)
+        // this.setState({coords: [e.clientX , e.clientY ]})
+
+        let tt = this.toolTip.current
+        console.log(tt)
+
         this.setState({hidden: !this.state.hidden})
     }
 
