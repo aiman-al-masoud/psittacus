94793a85b7ac34e8255019cfff26d2df4eb40028
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-05-13 19:46:39 +0200

    table headers misalligned but 'sticky'

--- psittacus/app/src/view/history/LessonsTable.jsx
+++ psittacus/app/src/view/history/LessonsTable.jsx
@@ -13,47 +13,49 @@ export default class LessonsTable extends Component {
     constructor(props) {
         super(props)
         this.props = props
-        
+
         this.state = {
-            metadataFilter : {
-                author : undefined,
+            metadataFilter: {
+                author: undefined,
                 targetLanguage: undefined,
                 sourceLanguage: undefined,
-                title:undefined
+                title: undefined
             }
         }
     }
 
-    onUpdateFilter = (field, newVal)=>{
-        
+    onUpdateFilter = (field, newVal) => {
+
         let m = this.state.metadataFilter
         m[field] = newVal
-        this.setState({metadataFilter: m })
+        this.setState({ metadataFilter: m })
     }
 
     render() {
         return (<div>
 
-            <div style={{overflowY:"scroll", height:"200px", width : "70vw"}}>
-            <table className="table" style={{width : "70vw"}}>
-                <tr><th>author</th><th>target language</th><th>source language</th><th>title</th></tr>
-                <tbody>
-                    {  Lesson.getLessonIdsHistory(this.state.metadataFilter).map(id => <LessonRow lessonId={id} key={id} takeLesson={this.props.takeLesson} />)}
-                </tbody>
-            </table>
+            <table className="table"  style={{width:"70vw"}}><tr><th>author</th><th>target language</th><th>source language</th><th>title</th></tr></table>
+
+            <div style={{ overflowY: "scroll", height: "200px", width: "70vw" }}>
+
+                <table className="table" style={{ width: "70vw" }}>
+                    <tbody>
+                        {Lesson.getLessonIdsHistory(this.state.metadataFilter).map(id => <LessonRow lessonId={id} key={id} takeLesson={this.props.takeLesson} />)}
+                    </tbody>
+                </table>
             </div>
 
             <h2>Filters</h2>
 
             Target Language:
-            <input type="text" onInput={(e)=>{ this.onUpdateFilter(  "target_language", e.target.value) }}/>
+            <input type="text" onInput={(e) => { this.onUpdateFilter("target_language", e.target.value) }} />
             Source Language:
-            <input type="text" onInput={(e)=>{ this.onUpdateFilter(  "source_language", e.target.value) }}/>
+            <input type="text" onInput={(e) => { this.onUpdateFilter("source_language", e.target.value) }} />
             <br />
             Author:
-            <input type="text" onInput={(e)=>{ this.onUpdateFilter(  "author", e.target.value) }}/>
+            <input type="text" onInput={(e) => { this.onUpdateFilter("author", e.target.value) }} />
             Title:
-            <input type="text" onInput={(e)=>{ this.onUpdateFilter(  "title", e.target.value) }}/>
+            <input type="text" onInput={(e) => { this.onUpdateFilter("title", e.target.value) }} />
             <br />
 
         </div>)
