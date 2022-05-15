bf269b8c71fb7614eadfd5564baa1e3fb59ee803
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-05-15 20:29:51 +0200

    extracted and translated string

--- psittacus/app/src/view/recycled/lessons_table/LessonRow.jsx
+++ psittacus/app/src/view/recycled/lessons_table/LessonRow.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Lesson from "../../../model/lesson/Lesson.js";
+import L from "../../../model/utilities/Language.js";
 
 export default class LessonRow extends Component {
 
@@ -25,6 +26,6 @@ export default class LessonRow extends Component {
     }
 
     render(){
-        return <tr tabIndex="0" title="click to open" style={{background: this.state.highlight? "yellow" : "white"  }}  onMouseEnter={ this.toggleHighlight } onMouseLeave={this.toggleHighlight}   onClick={   this.onTakeLesson  }    ><td>{Lesson.parseId(this.props.lessonId).author}</td><td>{Lesson.parseId(this.props.lessonId).target_language}</td><td>{Lesson.parseId(this.props.lessonId).source_language}</td><td>{Lesson.parseId(this.props.lessonId).title}</td> </tr>
+        return <tr tabIndex="0" title={L.click_to_open} style={{background: this.state.highlight? "yellow" : "white"  }}  onMouseEnter={ this.toggleHighlight } onMouseLeave={this.toggleHighlight}   onClick={   this.onTakeLesson  }    ><td>{Lesson.parseId(this.props.lessonId).author}</td><td>{Lesson.parseId(this.props.lessonId).target_language}</td><td>{Lesson.parseId(this.props.lessonId).source_language}</td><td>{Lesson.parseId(this.props.lessonId).title}</td> </tr>
     }
 }
