cd7992620dfa6525f41ea029872792a4d49adf6d
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-05-15 06:17:41 +0200

    ...

--- server/app.py
+++ server/app.py
@@ -1,10 +1,9 @@
-from flask import Flask                                                                                         
-                                                                                                                
-app = Flask(__name__)                                                                                           
-                                                                                                                
-path=app.root_path+"/../psittacus/dist/index.html"    
+from flask import Flask    
 
-with open(path, "r") as f:                                                                                      
+app = Flask(__name__)                                                                                                                                                                                                           
+webapp_path = app.root_path+"/../psittacus/dist/index.html"    
+
+with open(webapp_path, "r") as f:                                                                                      
     homepage=f.read()                                                                                               
                                                                                                                 
 @app.route('/')                                                                                                 
