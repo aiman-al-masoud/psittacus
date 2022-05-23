0ce9ea9fdaded5b39f0c854ea70f6f76fb9a39e1
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-05-23 15:03:20 +0200

    switch case in TakeLesson

--- psittacus/app/src/view/take_lesson/TakeLesson.jsx
+++ psittacus/app/src/view/take_lesson/TakeLesson.jsx
@@ -54,55 +54,52 @@ export default class TakeLesson extends Component {
         this.setState({ overallUserAccuracy: this.lesson.getScore() })
     }
 
-    //switch-case in here instead?
     render() {
 
-        return (<div>
+        this.main = (<div>
+            <h1>{L.translate_this_sentence}</h1>
+            <div className="text_tip">{L.need_a_tip_hover_words}</div>
+            <br />
 
-            <div style={this.state.mode == Modes.STANDARD ? Styles.visible : Styles.invisible}>
-                <h1>{L.translate_this_sentence}</h1>
+            <div style={{ width: "50vw" }}>
+
+                <HoverableSentence wordDict={this.state.proposition.getQuestionWordDict()} />
+                <button style={Styles.visibleInline} onClick={this.state.proposition.play} className="play_audio_button" style={(this.state.proposition.targetToNative && (this.state.mode != Modes.LESSON_OVER)) ? Styles.visibleInline : Styles.invisible} title={`${L.play_audio} (${L.shortcut_play_audio})`}>   <img src={PlayAudioIcon} />  </button>
+
+            </div>
+
+            <br />
+            <input ref={this.userInput} type="text" className="normal_textbox" />
+            <br />
+            <button onClick={this.next} className="normal_button">{this.state.solutionHidden ? L.see_solution : L.next}  </button>
+            <br />
+            <div style={this.state.solutionHidden ? Styles.invisible : Styles.visible}>
+                <h1>{L.solution}:</h1>
                 <div className="text_tip">{L.need_a_tip_hover_words}</div>
-                <br />
 
                 <div style={{ width: "50vw" }}>
 
-                    <HoverableSentence wordDict={this.state.proposition.getQuestionWordDict()} />
-                    <button style={Styles.visibleInline} onClick={this.state.proposition.play} className="play_audio_button" style={(this.state.proposition.targetToNative && (this.state.mode != Modes.LESSON_OVER)) ? Styles.visibleInline : Styles.invisible} title={`${L.play_audio} (${L.shortcut_play_audio})`}>   <img src={PlayAudioIcon} />  </button>
+                    <HoverableSentence wordDict={this.state.proposition.getAnswerWordDict()} />
+                    <button style={Styles.visibleInline} onClick={this.state.proposition.play} className="play_audio_button" style={((!this.state.proposition.targetToNative) && (this.state.mode != Modes.LESSON_OVER)) ? Styles.visibleInline : Styles.invisible} title={`${L.play_audio} (${L.shortcut_play_audio})`}>   <img src={PlayAudioIcon} />  </button>
 
                 </div>
 
-                <br />
-                <input ref={this.userInput} type="text" className="normal_textbox" />
-                <br />
-                <button onClick={this.next} className="normal_button">{this.state.solutionHidden ? L.see_solution : L.next}  </button>
-                <br />
-                <div style={this.state.solutionHidden ? Styles.invisible : Styles.visible}>
-                    <h1>{L.solution}:</h1>
-                    <div className="text_tip">{L.need_a_tip_hover_words}</div>
 
-                    <div style={{ width: "50vw" }}>
-
-                        <HoverableSentence wordDict={this.state.proposition.getAnswerWordDict()} />
-                        <button style={Styles.visibleInline} onClick={this.state.proposition.play} className="play_audio_button" style={((!this.state.proposition.targetToNative) && (this.state.mode != Modes.LESSON_OVER)) ? Styles.visibleInline : Styles.invisible} title={`${L.play_audio} (${L.shortcut_play_audio})`}>   <img src={PlayAudioIcon} />  </button>
-
-                    </div>
-
-
-                    <h2>{L.your_accuracy}: {this.state.userAccuracy}%</h2>
-                </div>
-                <br />
-                <span className="text_tip">{L.need_a_lot_of_tips} <button className="normal_link" onClick={() => { this.setState({ mode: Modes.EXPLANATION }) }}>{L.read_explanation}</button></span>
+                <h2>{L.your_accuracy}: {this.state.userAccuracy}%</h2>
             </div>
-
-            <div style={this.state.mode == Modes.LESSON_OVER ? Styles.visible : Styles.invisible}   >
-                <LessonOver overallUserAccuracy={this.state.overallUserAccuracy}/>
-            </div>
-
-            <div style={this.state.mode == Modes.EXPLANATION ? Styles.visible : Styles.invisible}   >
-                <Explanation explanationText={this.lesson.explanationText} onBack={ () => { this.setState({ mode: Modes.STANDARD }) } } />
-            </div>
-
+            <br />
+            <span className="text_tip">{L.need_a_lot_of_tips} <button className="normal_link" onClick={() => { this.setState({ mode: Modes.EXPLANATION }) }}>{L.read_explanation}</button></span>
         </div>)
+
+        switch (this.state.mode ) {
+            case Modes.STANDARD:
+                return this.main;
+            case Modes.EXPLANATION:
+                return <Explanation explanationText={this.lesson.explanationText} onBack={ () => { this.setState({ mode: Modes.STANDARD }) } } />
+            case Modes.LESSON_OVER:
+                return <LessonOver overallUserAccuracy={this.state.overallUserAccuracy}/>
+        }
+
     }
 
     componentDidMount() {
